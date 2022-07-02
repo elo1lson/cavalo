6abd5c79e9248e6834de9dacc1c0512918323c3c
elo1lson <eloilsonfontenele2@gmail.com>
2022-07-02 19:40:15 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const horse = document.querySelector('img')
+
+document.addEventListener('click',({target})=>{
+console.log(target.classList.contains('quiz-button'));
+if(target.classList.contains('quiz-button')){
+  
+}
+})
